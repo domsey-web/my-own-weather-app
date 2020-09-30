17e925d3c7da93464fd729ba95a045dc849c7bf4
domsey-web <71871267+domsey-web@users.noreply.github.com>
2020-09-30 18:20:53 +0100

    added date and time

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-// ~ Date & Time ~
-let dateElement = document.querySelector("#date-time");
-
-let currentTime = new Date();
-let hours = currentTime.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let minutes = currentTime.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-let dayIndex = currentTime.getDay();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-dateElement.innerHTML = `${days[dayIndex]}, ${hours}:${minutes}`;
-
 // ~ Celcius/Fahrenheit ~
 
 function search(event) {
@@ -52,6 +27,23 @@ function clickF(event) {
 let changeToFahrenheit = document.querySelector("#fahrenheit-link");
 changeToFahrenheit.addEventListener("click", clickF);
 
+// ~ Date & Time ~
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[date.getDay()];
+  return `${day}, ${hours}:${minutes} `;
+}
+
 // ~ Location & Temp Change ~
 
 function displayTemperature(response) {
@@ -61,14 +53,16 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windspeedElement = document.querySelector("#windspeed");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windspeedElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "d2b4efd6e0f5423f450f89aaf0181665";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Cardiff&units=metric&appid=${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=${apiKey}`;
 
 axios.get(apiUrl).then(displayTemperature);
